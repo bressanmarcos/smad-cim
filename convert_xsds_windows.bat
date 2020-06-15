d913b38aee911029a6cf93838a060133b521cb3b
bressanmarcos <bressanmarcos@alu.ufc.br>
2020-06-15 14:24:39 -0300

    Fix em Script para conversão Windows

--- convert_xsds_windows.bat
+++ convert_xsds_windows.bat
@@ -1,15 +1,6 @@
-REM DEFINIR AMBIENTE
-set myenv=smad
-
-REM MUDAR PARA AMBIENTE
-call %activate% %myenv%
-
-set activate="C:\Users\Bressan\Anaconda3\Scripts\activate.bat"
-set generateds="..\python\generateDS\generateDS.py"
-
 REM DEFINIR CAMINHOS
 set origin=artifacts
-set destination=information_modelgenerateDS -o "%destination%\%%~ni.py" --export="write etree validate generator" --no-questions --silence "%%i"
+set destination=information_model
 
 REM GERAR ESQUELETOS .PY PARA CADA .XSD -s "%destination%\%%~nisubs.py" --super="%super%.%%~ni" 
 for /r %%i in "%origin%\*.xsd" do (
