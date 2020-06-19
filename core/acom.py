e810abf7991daedfa98d1497e15d5f3e5821197c
Marcos Bressan <bressanmarcos@alu.ufc.br>
2020-06-19 19:16:58 -0300

    ACom (incomplete)

--- core/acom.py
+++ core/acom.py
@@ -13,8 +13,13 @@ from common import AgenteSMAD, to_elementtree, to_string
 
 
 class EnvioDeDados(FipaSubscribeProtocol):
+    """Permite o cadastro de assinantes (em especial, o ADC)
+    para que recebam mensagens relativas
+    à ocorrência de uma falta
+    """
+
     def __init__(self, agent: AgenteSMAD):
-        super(EnvioDeDados, self).__init__(agent, is_initiator=False)
+        super().__init__(agent, is_initiator=False)
 
     def handle_subscribe(self, message: ACLMessage):
         self.register(message.sender)
@@ -27,16 +32,30 @@ class EnvioDeDados(FipaSubscribeProtocol):
         display_message(self.agent.aid.name, 'Mensagem incompreendida')
 
 
+class ReceberComando(FipaRequestProtocol):
+    """Recebe lista de comandos (do ADC)
+    para manobra de chaves
+    *SwitchingPlan*
+    """
+
+    def __init__(self, agent: AgenteSMAD):
+        super().__init__(agent, is_initiator=False)
+
+    def handle_request(self, message):
+        """Recepção de mensagem de comando 
+        (conteúdo deve ser um SwitchingPlan)
+        """
+        pass
+
+
 class AgenteCom(AgenteSMAD):
 
-    def __init__(self, aid: AID, substation: str, debug=False):
+    def __init__(self, aid: AID, substation: str, enderecos_IEDs={}, debug=False):
         super().__init__(aid, substation, debug)
-        self.envio_behaviour = EnvioDeDados(self)
-        self.behaviours.append(self.envio_behaviour)
+        self.enderecos_IEDs = enderecos_IEDs
+        self.behaviours.append(EnvioDeDados(self))
+        self.behaviours.append(ReceberComando(self))
         # self.call_later(10, self.definir_mensagem)
-        """Testes
-        """
-        self.comandar_chave(switchId='Switch123', action='close')
 
     def comandar_chave(self, switchId=None, action='open'):
         """Implementa rotina low-level para use case Comandar chave
@@ -44,8 +63,9 @@ class AgenteCom(AgenteSMAD):
         Atualmente é somente um stub para controle de uma ÚNICA chave
         """
         value_to_write = 1 if action == 'open' else 2
+        endereco = self.enderecos_IEDs[switchId]
         display_message(
-            self.aid.name, f'Comando "{value_to_write}" ({action}) --> Chave "{switchId}"')
+            self.aid.name, f'Comando "{value_to_write}" ({action}) --> {switchId} [{endereco}]')
 
     def definir_mensagem(self):
         # Diagnosticar falta::Informar atuação de chaves
@@ -88,4 +108,28 @@ class AgenteCom(AgenteSMAD):
 
 
 if __name__ == "__main__":
-    a = AgenteCom(AID('acom1@localhost:10000'), 'S1')
+    enderecos_S1 = {"CH1": "192.168.0.101",
+                    "CH2": "192.168.0.102",
+                    "CH3": "192.168.0.103",
+                    "CH6": "192.168.0.106",
+                    "CH7": "192.168.0.107",
+                    "CH8": "192.168.0.108",
+                    "CH9": "192.168.0.109",
+                    "CH10": "192.168.0.110",
+                    "CH11": "192.168.0.111",
+                    "CH13": "192.168.0.113",
+                    "CH14": "192.168.0.114",
+                    "CH15": "192.168.0.115",
+                    "CH16": "192.168.0.116"}
+    enderecos_S2 = {"CH4": "192.168.0.104",
+                    "CH5": "192.168.0.105",
+                    "CH3": "192.168.0.103",
+                    "CH8": "192.168.0.108",
+                    "CH11": "192.168.0.111",
+                    "CH12": "192.168.0.112",
+                    "CH16": "192.168.0.116"}
+    enderecos_S3 = {"CH17": "192.168.0.117",
+                    "CH18": "192.168.0.118",
+                    "CH16": "192.168.0.116"}
+    acom = AgenteCom(AID('acom1@localhost:9000'), 'S1', enderecos_S1)
+    acom.comandar_chave('CH13', 'close')
