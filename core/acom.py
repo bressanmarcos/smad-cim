1bf537f2f3be7af3c642289ad4cad30488afe6af
Marcos Bressan <bressanmarcos@alu.ufc.br>
2020-06-20 20:37:27 -0300

    added SwitchingPlan example to ACom

--- core/acom.py
+++ core/acom.py
@@ -9,9 +9,12 @@ from pade.behaviours.protocols import (FipaRequestProtocol,
                                        FipaSubscribeProtocol, TimedBehaviour)
 from pade.misc.utility import display_message
 
-from common import AgenteSMAD, to_elementtree, to_string
+from common import AgenteSMAD, dump
 
-
+import sys
+sys.path.insert(0, '../')
+from information_model import SwitchingCommand as swc
+ 
 class EnvioDeDados(FipaSubscribeProtocol):
     """Permite o cadastro de assinantes (em especial, o ADC)
     para que recebam mensagens relativas
@@ -74,42 +77,7 @@ class AgenteCom(AgenteSMAD):
 
     def definir_mensagem(self):
         # Diagnosticar falta::Informar atuação de chaves
-        event_message = ACLMessage(ACLMessage.INFORM)
-        event_message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
-        event_message.set_ontology('outage')
-        event_message.set_datetime_now()
-
-        obj = outage.Outage_Type(uuid4())
-        switch = outage.ProtectedSwitch('switch' + str(uuid4()))
-        analog = outage.Analog(name=outage.Analog_Meas.CURRENT_MAGNITUDE)
-        analog.set_phases(outage.PhaseCode.A)
-        analog.set_unitMultiplier(outage.UnitMultiplier.K)
-        analog.set_unitSymbol(outage.UnitSymbol.A)
-        analog_value = outage.AnalogValue(value=random())
-        analog.set_AnalogValue(analog_value)
-        switch.add_Analog_Measurement(analog)
-        discrete = outage.Discrete(name=outage.Discrete_Meas.BREAKER_POSITION)
-        discrete.add_DiscreteValue(outage.DiscreteValue(
-            value=outage.Breaker_DiscreteValue_Integer(1)))
-        value_alias_set = outage.ValueAliasSet()
-        value_alias_set.add_Value(outage.ValueToAlias(
-            value=0, aliasName=outage.Breaker_DiscreteMeasAlias.INTERMEDIATE))
-        value_alias_set.add_Value(outage.ValueToAlias(
-            value=1, aliasName=outage.Breaker_DiscreteMeasAlias.OPEN))
-        value_alias_set.add_Value(outage.ValueToAlias(
-            value=2, aliasName=outage.Breaker_DiscreteMeasAlias.CLOSE))
-        value_alias_set.add_Value(outage.ValueToAlias(
-            value=3, aliasName=outage.Breaker_DiscreteMeasAlias.INVALID))
-
-        discrete.set_ValueAliasSet(value_alias_set)
-        switch.add_Discrete_Measurement(value=discrete)
-        obj.add_ProtectedSwitch(switch)
-        event_message.set_content(obj)
-
-        event_message.as_xml()
-
-        self.envio_behaviour.notify(event_message)
-        self.call_later(2, self.main)
+        pass
 
 
 if __name__ == "__main__":
@@ -138,3 +106,22 @@ if __name__ == "__main__":
                     "CH16": "192.168.0.116"}
     acom = AgenteCom(AID('acom1@localhost:9000'), 'S1', enderecos_S1)
     acom.comandar_chave('CH13', 'close')
+    #################################################
+    # Exemmplo de Arquivo para ser recebido pelo ACOM
+    swt = swc.ProtectedSwitch(mRID='CH13', name='Switch que protege minha casa')
+    acao = swc.SwitchAction(
+        executedDateTime=datetime.datetime.now(), 
+        isFreeSequence=True, 
+        issuedDateTime=datetime.datetime.now(), 
+        kind=swc.SwitchActionKind.CLOSE, 
+        plannedDateTime=datetime.datetime.now(), 
+        sequenceNumber=0, 
+        OperatedSwitch=swt)
+    plano = swc.SwitchingPlan(
+        mRID='PlanID', 
+        createdDateTime=datetime.datetime.now(),
+        name='Plano de Teste', 
+        purpose=swc.Purpose.COORDINATION, 
+        SwitchAction=[acao])
+    root = swc.SwitchingCommand(SwitchingPlan=plano)
+    dump(root.to_etree())
