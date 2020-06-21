168903d79db92196be7c0c69851f75bb228a7530
Marcos Bressan <bressanmarcos@alu.ufc.br>
2020-06-20 22:12:13 -0300

    Documentation in common.py

--- core/common.py
+++ core/common.py
@@ -1,3 +1,6 @@
+"""Contém funções e classes úteis
+"""
+
 from pade.core.agent import Agent
 from pade.acl.aid import AID
 from pade.acl.messages import ACLMessage
@@ -7,6 +10,8 @@ from xml.dom.minidom import parseString
 
 
 def to_elementtree(document, name_=None):
+    """Converte elemento do generateDS em ElementTree
+    """
     etree = document.to_etree() if name_ == None else document.to_etree(name_=name_)
     try:
         return ET.fromstring(lET.tostring(etree))
@@ -14,15 +19,18 @@ def to_elementtree(document, name_=None):
         return etree
 
 
-def to_string(element):
+def to_string(elementtree):
+    """Converte um ElementTree em string"""
     try:
-        return ET.tostring(element)
+        return ET.tostring(elementtree)
     except:
-        return lET.tostring(element)
+        return lET.tostring(elementtree)
 
 
-def dump(element):
-    rough_string = to_string(element)
+def dump(elementtree):
+    """Imprime ElementTree no stdout 
+    """
+    rough_string = to_string(elementtree)
     reparsed = parseString(rough_string)
     print(reparsed.toprettyxml(indent=' '*4))
 
