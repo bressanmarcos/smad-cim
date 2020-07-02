20c101c193b110f28576ee9c74b4845e51136be5
Marcos Bressan <bressanmarcos@alu.ufc.br>
2020-07-02 20:43:02 -0300

    Adiciona notificação de dados de IED
    - Dados dos IEDs recebidos são lidos de arquivos com mesmo nome
    - Deadtime implementado (3.0 segundos) no ACom para reunir mensagens dos IEDs
    antes de enviá-las ao ADC
    - Frequência de leitura de arquivos dos IEDs alterada para 1 segundo
    - Testes em 'test_adc.py' para assinatura de ADC a ACom e recepção
    de 'notify'

--- core/acom.py
+++ core/acom.py
@@ -9,12 +9,14 @@ from pade.behaviours.protocols import (FipaRequestProtocol,
                                        FipaSubscribeProtocol, TimedBehaviour)
 from pade.misc.utility import display_message
 
-from core.common import AgenteSMAD, to_elementtree, to_string, dump
+from core.common import AgenteSMAD, to_elementtree, to_string, dump, validate
 from core.ied import IED
 
 import sys
 sys.path.insert(0, '../')
 from information_model import SwitchingCommand as swc
+from information_model import OutageEvent as out
+
  
 class EnvioDeDados(FipaSubscribeProtocol):
     """Permite o cadastro de assinantes (em especial, o ADC)
@@ -118,11 +120,22 @@ class AgenteCom(AgenteSMAD):
 
     def __init__(self, aid: AID, substation: str, enderecos_IEDs={}, debug=False):
         super().__init__(aid, substation, debug)
+
+        # Instancia IEDs
         self.IEDs = {}
         for (_id, ip) in enderecos_IEDs.items():
             self.IEDs[_id] = IED(_id, ip, call_on_event=self.receber_evento, initial_breaker_position='close')
-        self.behaviours.append(EnvioDeDados(self))
-        self.behaviours.append(ReceberComando(self))
+
+        # Adiciona behaviours
+        self.behaviours_enviodedados = EnvioDeDados(self) # Permite enviar dados ao ADC
+        self.behaviours.append(self.behaviours_enviodedados) 
+        self.behaviours_recebercomando = ReceberComando(self) # Permite receber comandos do ADC
+        self.behaviours.append(self.behaviours_recebercomando)
+
+        self.deadtime = 3.0 # Tempo em segundos do deadtime
+        self.in_hold = False # Mantém o ACom à escuta durante um certo tempo de deadtime antes de notificar eventos
+        self.document_to_send = None # Armazena temporariamente o documento enquanto ele está sendo construído
+
 
     def on_start(self):
         super().on_start()
@@ -132,12 +145,81 @@ class AgenteCom(AgenteSMAD):
 
     def receber_evento(self, *args):
         """Função invocada quando ACom recebe mensagem do IED. Formato de entrada: \\
-        ``args = ('PTOC', 'XCBR', BRKF')`` \\
+        ``args = (<IED instance>, 'PTOC', 'XCBR', 'BRKF')`` \\
         Mensagens dos recebidas pelo ACom são reunidas durante um ``deadtime``
         antes de serem todas encaminhadas (no formato adequado) ao ADC."""
-        # TODO: Para UC de diagóstico de Chaves
-        display_message(self.aid.name, f'Evento recebido: {args}')
-        pass
+        display_message(self.aid.name, f'Evento recebido do IED: {args[0].id} {args[1:]}')
+
+        # Verifica se ACom já está à escuta de um conjunto de mensagens
+        # (se já recebeu alguma mensagem durante o deadtime ou não)
+        def send_document():
+            # Sai do hold e envia documento
+            self.in_hold = False
+            # Cria pacote de mensagem e envia ao ADC
+            message = ACLMessage(performative=ACLMessage.INFORM)
+            message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
+            message.set_content(to_elementtree(self.document_to_send))
+            self.behaviours_enviodedados.notify(message)
+
+        if not self.in_hold:
+            # Caso não esteja em deadtime, entra.
+            self.in_hold = True
+            # Cria novo documento
+            outage = out.Outage()
+            self.document_to_send = out.OutageEvent(Outage=outage)
+            # Define o deadtime para enviar documento em 6 segundos
+            self.call_later(self.deadtime, send_document)
+
+        # Converte informações recebidas em formato CIM XML
+        switchId = args[0].id
+        switch = out.ProtectedSwitch(
+            mRID=switchId, 
+            normalOpen=False)
+        for function in args[1:]:
+            if function == 'XCBR':
+                # Código do retorno
+                value = out.Breaker_DiscreteValue_Integer('1')
+                discrete_value = out.DiscreteValue(value=value)
+                # Significado dos códigos
+                value_to_alias_0 = out.ValueToAlias(
+                    value=out.Breaker_DiscreteValue_Integer('0'), 
+                    aliasName=out.Breaker_DiscreteMeasAlias.INVALID)
+                value_to_alias_1 = out.ValueToAlias(
+                    value=out.Breaker_DiscreteValue_Integer('1'), 
+                    aliasName=out.Breaker_DiscreteMeasAlias.OPEN)
+                value_to_alias_2 = out.ValueToAlias(
+                    value=out.Breaker_DiscreteValue_Integer('2'), 
+                    aliasName=out.Breaker_DiscreteMeasAlias.CLOSE)
+                value_to_alias_3 = out.ValueToAlias(
+                    value=out.Breaker_DiscreteValue_Integer('3'), 
+                    aliasName=out.Breaker_DiscreteMeasAlias.INTERMEDIATE)
+                value_alias_set = out.ValueAliasSet(Value=[value_to_alias_0, value_to_alias_1, value_to_alias_2, value_to_alias_3])
+                # Insere na medição
+                discrete = out.Discrete(
+                    name=out.Discrete_Meas.BREAKER_POSITION, 
+                    DiscreteValue=discrete_value,
+                    ValueAliasSet=value_alias_set)
+                switch.add_Discrete_Measurement(discrete)
+            elif function in 'BRKF':
+                # Código do retorno
+                value = out.Breaker_DiscreteValue_Integer('1')
+                discrete_value = out.DiscreteValue(value=value)
+                # Significado dos códigos
+                value_to_alias_0 = out.ValueToAlias(
+                    value=out.Breaker_DiscreteValue_Integer('0'), 
+                    aliasName=out.Breaker_DiscreteMeasAlias.FALSE)
+                value_to_alias_1 = out.ValueToAlias(
+                    value=out.Breaker_DiscreteValue_Integer('1'), 
+                    aliasName=out.Breaker_DiscreteMeasAlias.TRUE)
+                value_alias_set = out.ValueAliasSet(Value=[value_to_alias_0, value_to_alias_1])
+                discrete = out.Discrete(
+                    name=out.Discrete_Meas.BREAKER_FAILURE, 
+                    DiscreteValue=discrete_value,
+                    ValueAliasSet=value_alias_set)
+                switch.add_Discrete_Measurement(discrete)
+
+        # Adiciona Switch ao documento temporário
+        self.document_to_send.get_Outage().add_ProtectedSwitch(switch)
 
     def comandar_chave(self, switchId=None, action='open'):
         """Chama a instância do IED para operar chave. 
