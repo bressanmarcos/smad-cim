c5f53432f32a5d95f928acf732f012474f1245e8
Alexandre Albano <alexandrealbano@me.com>
2020-07-20 05:50:38 -0300

    mais adaptacoes do cod Tiago

--- core/adc.py
+++ core/adc.py
@@ -1,5 +1,8 @@
 import os
-os.sys.path.insert(0, os.getcwd()) 
+
+from rede.IPs_IEDs import enderecos_IEDs
+
+os.sys.path.insert(0, os.getcwd())
 # Adiciona ao Path a pasta raiz do projeto
 
 import datetime
@@ -18,15 +21,17 @@ from information_model import OutageEvent as out
 
 from rede.rdf2mygrid import carregar_topologia
 
+from rede.IPs_IEDs import enderecos_IEDs
+
 
 class SubscreverACom(FipaSubscribeProtocol):
     def __init__(self, agent: AgenteSMAD, message=None, is_initiator=True):
         super().__init__(agent, message=message, is_initiator=is_initiator)
-        
+
     def handle_agree(self, message):
         display_message(self.agent.aid.name, 'Inscrito em ACom')
 
-    def handle_inform(self, message: ACLMessage):    
+    def handle_inform(self, message: ACLMessage):
         """Receve notificação de evento do ACom. \\
         ``message.content`` é recebida no formato OutageEvent
         """
@@ -55,16 +60,18 @@ class SubscreverACom(FipaSubscribeProtocol):
                         lista_de_chaves[switchId].append('breaker_failure')
 
         print(lista_de_chaves)
-        #lista de chaves para teste
-        lista_teste = {'chaves': ['CH14', 'CH13'], 'leitura_falta': ['CH14', 'CH13'], 'ctime': 'Fri Jul  3 18:20:58 2020'}
+        #***** juntar a mensagem recebida das IED com o codigo do Tiago
+        # lista de chaves para teste
+        lista_teste = {'chaves': ['CH14', 'CH13'], 'leitura_falta': ['CH14', 'CH13'],
+                       'ctime': 'Fri Jul  3 18:20:58 2020'}
 
-        #Inicio da analise de descoordenacao
+        # Inicio da analise de descoordenacao
         dados_falta = self.agent.analise_descoordenacao(lista_teste)
         print(dados_falta)
         content = {"dados": dados_falta}
         if dados_falta["coordenado"] == False:
-        #Corrigir descoordenacao
-        #ALE: antigo agente de controle
+            # Corrigir descoordenacao
+            # ALE: antigo agente de controle
             content2 = dict()
             content2["chave_falta"] = content["dados"]["chave_falta"]
             display_message(self.agent.aid.name, "Iniciando correcao de Descoordenacao")
@@ -138,12 +145,12 @@ class SubscreverACom(FipaSubscribeProtocol):
                 display_message(self.agent.aid.name,
                                 "Impossivel corrigir descoordenacao.")
             print("final do ant ag controle:" + str(content2))
-        #ALE: Final antigo agente de controle
-            #Voltando para o Agente diagnostico
+            # ALE: Final antigo agente de controle
+            # Voltando para o Agente diagnostico
             content = self.agent.analise_isolamento(content2["chave_falta"])
             print("voltando para o ag diag: " + str(content))
-            #Final do Agente diagnostico
-            #Voltando para o agente de controle
+            # Final do Agente diagnostico
+            # Voltando para o agente de controle R_03
             # Indica inicio da analise
             display_message(self.agent.aid.name, "Iniciando Isolamento de Trecho")
             # Para todas as chaves que devem ser
@@ -161,7 +168,9 @@ class SubscreverACom(FipaSubscribeProtocol):
                         "Chave " + chave + " nao possui IP cadastrado"))
                     content["isolamento_realizado"] = False
             print(content)
-            #final agente de controle
+            # final agente de controle R_03
+            #Agente diagnostico R_03 CompRequest3
+            self.agent.analise_recomposicao(content)
 
         else:
             self.agent.analise_isolamento(dados_falta["chave_falta"])
@@ -190,21 +199,21 @@ class AgenteDC(AgenteSMAD):
         self.behaviours.append(EnviarComando(self))
         display_message(self.aid.name, "Agente instanciado")
 
-        #Inicio cod Tiago para o agente diagnostico
+        # Inicio cod Tiago para o agente diagnostico
         self.subestacao = subestacao
         self.enderecos_IEDs = enderecos_IEDs[subestacao]
         self.relatorios_restauracao = list()
         self.topologia_subestacao = carregar_topologia('./rede/rede-cim.xml', subestacao)
 
-        display_message(self.aid.name,"Subestacao {SE} carregada com sucesso".format(SE=subestacao))
+        display_message(self.aid.name, "Subestacao {SE} carregada com sucesso".format(SE=subestacao))
         self.podas = list()
         self.podas_possiveis = list()
         self.setores_faltosos = list()
-        #comportamento_requisicao = CompRequest1(self)
-        #self.behaviours.append(comportamento_requisicao)
-        #comp_contractnet_participante = CompContractNet1(self)
-        #self.behaviours.append(comp_contractnet_participante)
-        #Final cod Tiago para o agente diagnostico
+        # comportamento_requisicao = CompRequest1(self)
+        # self.behaviours.append(comportamento_requisicao)
+        # comp_contractnet_participante = CompContractNet1(self)
+        # self.behaviours.append(comp_contractnet_participante)
+        # Final cod Tiago para o agente diagnostico
 
     def enviar_comando_de_chave(self, switching_command: swc.SwitchingCommand, acom_aid: AID):
         """Envia um objeto de informação do tipo SwitchingCommand ao ACom fornecido"""
@@ -216,6 +225,7 @@ class AgenteDC(AgenteSMAD):
         message.add_receiver(acom_aid)
         message.set_ontology('SwitchingCommand')
         message.set_content(to_elementtree(switching_command))
+
         def later():
             # Se o ACom já estiver na tabela
             if hasattr(self, 'agentInstance') and acom_aid.name in self.agentInstance.table:
@@ -224,8 +234,8 @@ class AgenteDC(AgenteSMAD):
             else:
                 # Reenvia mensagem 5 segundos mais tarde
                 self.call_later(5.0, later)
-        later()
 
+        later()
 
     def subscribe_to(self, acom_aid: AID):
         """Subcribe to ``AgenteCom``"""
@@ -234,6 +244,7 @@ class AgenteDC(AgenteSMAD):
         message.add_receiver(acom_aid)
         self.subscribe_behaviour = SubscreverACom(self, message, is_initiator=True)
         self.behaviours.append(self.subscribe_behaviour)
+
         def later():
             if hasattr(self, 'agentInstance') and acom_aid.name in self.agentInstance.table:
                 # Envia mensagem
@@ -241,9 +252,10 @@ class AgenteDC(AgenteSMAD):
             else:
                 # Reenvia mensagem mais tarde
                 self.call_later(2.0, later)
+
         later()
 
-    #Inicio Cod Tiago
+    # Inicio Cod Tiago
     def localizar_chave(self, chave_busca):
         for alimentador in self.topologia_subestacao.alimentadores.keys():
             if chave_busca in self.topologia_subestacao.alimentadores[alimentador].chaves.keys():
@@ -407,14 +419,26 @@ class AgenteDC(AgenteSMAD):
             content = dict()
             content["dados"] = dados_isolamento
 
-            #***Comandar chaves
+            # ***Comandar chaves antigo R_03
             print("Dentro da funcao analise isolamento: " + str(dados_isolamento))
+            display_message(self.aid.name, "Iniciando Isolamento de Trecho")
+            display_message(self.aid.name, "***R_03 = " + str(content))
+            for chave in content["dados"]["chaves"]:
+                # Verifica se a chave faz parte da SE com conexo TCP-IP
+                if chave in self.enderecos_IEDs.keys():
+                    display_message(self.aid.name, str(
+                        "Isolando Trecho Defeituoso [CH:" + chave + "]"))
+                    content["isolamento_realizado"] = True
+                else:
+                    display_message(self.aid.name, str(
+                        "Chave " + chave + " nao possui IP cadastrado"))
+                    content["isolamento_realizado"] = False
+
             return dados_isolamento
 
         else:
             display_message(self.aid.name, "Nenhum setor precisa ser isolado")
             self.analise_recomposicao(dados_isolamento)
-
 
     def analise_recomposicao(self, dados_isolamento=dict):
         display_message(self.aid.name, "------------------------")
@@ -441,8 +465,7 @@ class AgenteDC(AgenteSMAD):
                         # Faz uma varredura nas chaves da poda e verifica se a chave pertence ao alimentador
                         # do laco for em questao (diferente do alimentador faltoso)
                         for chave in poda[6].keys():
-                            if chave in self.topologia_subestacao.alimentadores[
-                                alimentador].chaves.keys():  # Pertence
+                            if chave in self.topologia_subestacao.alimentadores[alimentador].chaves.keys():  # Pertence
                                 display_message(self.aid.name,
                                                 "Possivel Restauracao de Ramo {i} pela mesma SE atraves de [CH: {ch}]".format(
                                                     i=i + 1, ch=chave))
@@ -467,18 +490,18 @@ class AgenteDC(AgenteSMAD):
                         for no in poda[0][3].keys():
                             content["nos_de_carga"][no] = round(poda[0][3][no].potencia.mod / 1000, 0)
                         # Elabora mensagem a ser enviada para o Acontrole correspondente
-                        #***verificar proximas linhas
+                        # ***verificar proximas linhas R_04
                         # Lanca Comportamento Request Iniciante
-                        #comp_mesma_se = CompRequest4(self, message)
-                        #self.behaviours.append(comp_mesma_se)
-                        #comp_mesma_se.on_start()
+                        # comp_mesma_se = CompRequest4(self, message)
+                        # self.behaviours.append(comp_mesma_se)
+                        # comp_mesma_se.on_start()
                         # Atualiza a lista de podas do agente
                         self.podas.remove(poda[0])
 
             # Pega todos os dados de recomposicao que nao foram restaurados na mesma SE
             # e envia ao Agente Negociacao para que ele comece os ciclos de negociacao
 
-            #***Refazer a comunicacao das proximas linhas
+            # ***Refazer a comunicacao das proximas linhas
             # content2 = dict()
             # content2["ramos"] = self.podas
             # message2 = ACLMessage(ACLMessage.REQUEST)
@@ -494,4 +517,4 @@ class AgenteDC(AgenteSMAD):
             display_message(self.aid.name, "Restauracao nao pode ser realizada [Falta nao isolada]")
         else:
             display_message(self.aid.name, "Falta em Final de Trecho")
-        #Final cod Tiago
+        # Final cod Tiago
