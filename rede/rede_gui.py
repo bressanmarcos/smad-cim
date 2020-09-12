974050d09921527f56190230a56cf33da110639e
Marcos Bressan <bressanmarcos@alu.ufc.br>
2020-09-11 22:16:02 -0300

    GUI integrada

--- rede/rede_gui.py
+++ rede/rede_gui.py
@@ -1,9 +1,12 @@
 
-from tkinter import *
+import os
+os.sys.path.insert(0, os.getcwd())
 
 import threading
+from tkinter import *
 
-from rede_simu import Network
+from rede.rede_simu import Network
+
 
 def str2int(string):
     return int(''.join(c for c in string if c.isdigit()))
@@ -31,17 +34,17 @@ def update_fields(window, network, fields):
         switch_state = network.read(switch_name)
         button.configure(bg = switch_button_colors[switch_state])
 
-    # Trechos
-    for segment_name, text_area in segment_texts.items():
-        segment_current = network.read_segments_load(segment_name)
-        value = round(segment_current.mod, 2)
-        text_area.config(text=f'{segment_name}: {value} A')
+    # # Trechos
+    # for segment_name, text_area in segment_texts.items():
+    #     segment_current = network.read_segments_load(segment_name)
+    #     value = round(segment_current.mod, 2)
+    #     text_area.config(text=f'{segment_name}: {value} A')
     
-    # Nos
-    for consumer_name, text_area in consumers_texts.items():
-        consumer_voltage = network.read_energy_consumers_voltage(consumer_name)
-        value = round(consumer_voltage.mod, 2)
-        text_area.config(text=f'{consumer_name}: {value} V')
+    # # Nos
+    # for consumer_name, text_area in consumers_texts.items():
+    #     consumer_voltage = network.read_energy_consumers_voltage(consumer_name)
+    #     value = round(consumer_voltage.mod, 2)
+    #     text_area.config(text=f'{consumer_name}: {value} V')
 
     # Keep updating
     window.after(100, lambda: update_fields(window, network, fields))
@@ -49,7 +52,9 @@ def update_fields(window, network, fields):
 if __name__ == "__main__":
 
     # Instancia rede
-    network = Network('./rede/models/rede-cim-2.xml')
+    network_filename = os.sys.argv[1]
+    ip_ied_filename = os.sys.argv[2]
+    network = Network(network_filename, ip_ied_filename)
  
     # Configura GUI
     window = Tk()
@@ -58,7 +63,7 @@ if __name__ == "__main__":
 
     # Chaves
     switch_buttons = {}
-    for index, chave in enumerate(sorted(network.chaves.values(), key=lambda chave: str2int(chave.nome))):
+    for index, chave in enumerate(sorted(network.chaves.values(), key=lambda chave: chave.nome)):
         button_name = chave.nome
         button = Button(window, text=button_name, command=(lambda name=button_name: change_state(network, name)))
         button.grid(column=0, row=index)
@@ -66,20 +71,20 @@ if __name__ == "__main__":
 
     # Trechos
     segment_texts = {}
-    for index, trecho in enumerate(network.trechos.values()):
-        nome_trecho = trecho.nome
-        text = Label(window)
-        text.grid(column=1+index//len(switch_buttons), row=index%len(switch_buttons))
-        segment_texts[nome_trecho] = text
+    # for index, trecho in enumerate(network.trechos.values()):
+    #     nome_trecho = trecho.nome
+    #     text = Label(window)
+    #     text.grid(column=1+index//len(switch_buttons), row=index%len(switch_buttons))
+    #     segment_texts[nome_trecho] = text
 
     # Nos
-    last_column = 2+index//len(switch_buttons)
+    # last_column = 2+index//len(switch_buttons)
     consumers_texts = {}
-    for index, no in enumerate(network.nos.values()):
-        nome_no = no.nome
-        text = Label(window)
-        text.grid(column=last_column+index//len(switch_buttons), row=index%len(switch_buttons))
-        consumers_texts[nome_no] = text
+    # for index, no in enumerate(network.nos.values()):
+    #     nome_no = no.nome
+    #     text = Label(window)
+    #     text.grid(column=last_column+index//len(switch_buttons), row=index%len(switch_buttons))
+    #     consumers_texts[nome_no] = text
     
     # Executa rede em novo processo
     p = threading.Thread(target=network.run)
--- rede/rede_simu.py
+++ rede/rede_simu.py
@@ -12,31 +12,29 @@ from pprint import pprint
 from rede.rdf2mygrid import carregar_topologia
 from mygrid.fluxo_de_carga.varred_dir_inv import calcular_fluxo_de_carga
 
-class Network():
+import imp, numpy as np
 
-    def __init__(self, filename):
+class Network():
+    ESTADO_CODIGO = {'open': 0, 'close': 1}
+    ESTADO_CODIGO_REVERSO = {value: key for key, value in ESTADO_CODIGO.items()}
+
+    def __init__(self, filename, ip_filename):
         self.filename = filename
         self.io_lock = Lock()
         self.reset_network()
+        self.ip_ied = imp.load_source('ip', ip_filename).ip_ied
+        self.ip_ied_reverse_map = {value: key for key, value in self.ip_ied.items()}
         
         # Abre conexões com os sockets das chaves
         socket_chaves = []
-        for nome, _ in self.chaves.items():
-            port = 50000 + int(nome.split('CH')[1])
+        for nome in self.chaves:
             sock = socket.socket()
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind(('localhost', port))
-            sock.listen()
+            sock.bind(self.ip_ied[nome])
             sock.setblocking(0)
+            sock.listen()
             socket_chaves.append(sock)
 
-        # Abre um socket para manipular eventos
-        socket_evento = socket.socket()
-        socket_evento.bind(('localhost', 50000))
-        socket_evento.listen()
-        socket_evento.setblocking(0)
-
-        self.socket_evento = socket_evento
         self.sockets_chaves = socket_chaves
  
     def reset_network(self):
@@ -55,9 +53,9 @@ class Network():
         readable, writable, exceptional = select.select(inputs, outputs, inputs, 0.2)
 
         for server in readable:
-            if server in self.sockets_chaves or server is self.socket_evento:
+            if server in self.sockets_chaves:
                 connection, client_address = server.accept()
-                print(f'Connection from {client_address}')
+                print(f'Connection from {client_address} to {server.getsockname()}')
                 connection.setblocking(0)
                 inputs.append(connection)
                 message_queues[connection] = queue.Queue()
@@ -81,21 +79,12 @@ class Network():
                 message = str(message_queues[server].get_nowait().strip(), 'utf-8').split(':')
                 command = message.pop(0)
 
-                if port == 50000:
-                    # Simulação de eventos
-                    switch = message.pop(0)
-                elif port > 50000:
-                    # Comando para IEDs
-                    switch = f'CH{port-50000}'
+                # Comando para IEDs
+                switch = self.ip_ied_reverse_map[('localhost', port)]
 
                 if command in ('read', 'operate'):
                     # Aciona comando se for Read ou Operate
                     next_msg = bytes(getattr(self, command)(switch, *message), 'utf-8')
-                
-                elif command == 'reset' and port == 50000:
-                    # Reseta estado da rede
-                    self.reset_network()
-                    next_msg = b'ok'
 
             except queue.Empty:
                 outputs.remove(server)
@@ -143,7 +132,7 @@ class Network():
         pass
 
     def run(self):
-        inputs = [self.socket_evento] + self.sockets_chaves
+        inputs = self.sockets_chaves[:]
         outputs = []
         message_queues = {}
         
@@ -160,24 +149,69 @@ class Network():
 
     def write(self, switch, action):
         with self.io_lock:
-            estados = {'open': 0, 'close': 1}
-            self.chaves[switch].estado = estados[action]
+            estado_atual = Network.ESTADO_CODIGO_REVERSO[self.chaves[switch].estado]
 
-            # if action == 'open':
-            #     def localizar_setor(self, setor = str):
-            #         for se in self.topologia_subestacao:
-            #             for alim in self.topologia_subestacao[se].alimentadores.keys():
-            #                 if setor in self.topologia_subestacao[se].alimentadores[alim].setores.keys():
-            #                     return self.topologia_subestacao[se].alimentadores[alim]
+            def localizar_chave(chave):
+                for se in self.subestacoes:
+                    for alim in self.subestacoes[se].alimentadores.keys():
+                        if chave in self.subestacoes[se].alimentadores[alim].chaves.keys():
+                            return self.subestacoes[se].alimentadores[alim]
 
-            #     setor_nome = self.chaves[switch].n2.nome
-            #     alim = localizar_setor(setor_nome)
-            #     self.podas.append(alim.podar(setor_nome, alterar_rnp=True))
+            def localizar_setor(setor):
+                for se in self.subestacoes:
+                    for alim in self.subestacoes[se].alimentadores.keys():
+                        if setor in self.subestacoes[se].alimentadores[alim].setores.keys():
+                            return self.subestacoes[se].alimentadores[alim]  
             
-            # elif action == 'close':
-            #     poda = self.podas.pop()
-            #     for n in ('n1', 'n2'):
-            #         pass
+            if estado_atual == 'close' and action == 'open':
+                # Busca o setor vizinho à chave que será podado
+                # alim = localizar_chave(switch)
+                # valor_mais_profundo = -1
+                # setor_mais_profundo = None
+                # for n in ('n1', 'n2'):
+                #     setor = getattr(self.chaves[switch], n).nome
+                #     prof = np.where(alim.rnp[1, :] == setor)[0][0]
+                #     if prof > valor_mais_profundo:
+                #         valor_mais_profundo = prof
+                #         setor_mais_profundo = setor
+
+                # Muda estado da chave
+                self.chaves[switch].estado = Network.ESTADO_CODIGO[action]
+
+                # Salva a poda
+                # poda = alim.podar(setor_mais_profundo, alterar_rnp=True)
+                # self.podas.append(poda)
+
+            
+            elif estado_atual == 'open' and action == 'close':
+                # Busca a poda salva que contém o switch recém aberto
+                # poda_buscada = None
+                # for poda in self.podas:
+                #     if switch in poda[6]:
+                #         poda_buscada = poda
+
+                # # Recupera a poda salva
+                # try:
+                #     poda = self.podas.pop(poda_buscada)
+                # except KeyError:
+                #     print('Short-circuit between feeders!')
+
+                # # Busca o setor vizinho à chave, que alimentará a poda
+                # setor_colab = None
+                # for n in ('n1', 'n2'):
+                #     setor = getattr(poda[6], n).nome
+                #     if setor not in poda[0]:
+                #         setor_colab = setor
+                #         n_ = next(n_ for n_ in ('n1', 'n2') if n != n_)
+                #         setor_raiz = getattr(poda[6], n_).nome
+                # alim = localizar_setor(setor_colab)
+
+                # Muda estado da chave
+                self.chaves[switch].estado = Network.ESTADO_CODIGO[action]
+
+                # Insere poda ao novo alimentador
+                # alim.inserir_ramo(setor_colab, poda, setor_raiz)
+
 
     def read(self, switch):
         """Read the current state of a switch remotely"""
@@ -197,4 +231,4 @@ class Network():
             return self.nos[no].tensao
 
 if __name__ == "__main__":
-    Network(os.path.dirname(__file__) + '/models/rede-cim.xml').run()
+    Network(os.path.dirname(__file__) + '/models/new-network.xml', '').run()
